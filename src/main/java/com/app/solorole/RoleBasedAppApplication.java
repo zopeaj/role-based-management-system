f02fa827efa729770ecd7fb823e89d44636d5b4f
Ezekiel <zopeaj@gmail.com>
2023-03-27 05:59:45 +0100

    role based app updated

---
+++ src/main/java/com/app/solorole/RoleBasedAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.solorole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleBasedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleBasedAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/solorole/RoleBasedAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.solorole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBasedAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
